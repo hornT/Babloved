42bd4fb381314f1c81fc536e968571a767e16c82
nikolay neverov <nikolay.neverov@ledgerleopard.com>
2020-02-11 15:01:20 +0300

    add koa

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const { getCurrency } = require('./currency');
 const bot = require('./bot');
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
 
 module.exports.bot = async (event) => {
   
