e28ed9beee4e06e123f7141fce1efc11c2178795
nikolay neverov <nikolay.neverov@ledgerleopard.com>
2020-02-13 10:17:31 +0300

    clear db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+const config = require('./config');
+
+
+client.on("error", function(error) {
+    console.error(error);
+  });
+
+function getRate(){
+}
+
+function setRate(rate){
+}
+
+module.exports = {
+    getRate,
+    setRate,
+}
